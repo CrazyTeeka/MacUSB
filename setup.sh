d8ef787ea459b7bf35d286cea84a3648ed4cd092
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-13 14:20:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p Clover/5116
+mkdir -p Clover/5116/EFI
 mkdir -p Clover.Config/Config
 mkdir -p Clover.Config/DSDT
-mkdir -p Clover.Config/Themes
-mkdir -p Clover.User/James
-mkdir -p Clover.User/Frank
+mkdir -p Clover.Config/Themes/Clovy
+mkdir -p Clover.User/James/EFI
+mkdir -p Clover.User/Frank/EFI
 mkdir -p Scripts
 mkdir -p Tools
