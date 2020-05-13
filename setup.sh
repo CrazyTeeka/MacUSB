dd020eaffd56c153669ac43eef4da6db8450214b
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-13 14:05:22 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p Clover
+mkdir -p Clover.Config/Config
+mkdir -p Clover.Config/DSDT
+mkdir -p Clover.Config/Themes
+mkdir -p Clover.User/James
+mkdir -p Clover.User/Frank
+mkdir -p Scripts
+mkdir -p Tools
