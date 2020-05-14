0fce12ad012f4373a31ea56bd05c9583d456a25d
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 14:36:04 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -11,30 +11,30 @@ elif [ "$1" = "10.15.4" ]; then URL="http://swcdn.apple.com/content/downloads/37
 else exit 0
 fi
 
-mkdir -p ../MacOS/$1
-rm -rf   ../MacOS/$1/*
+mkdir -p ~/MacOS/$1
+rm -rf   ~/MacOS/$1/*
 
 echo "Downloading AppleDiagnostics.dmg..."
-wget -O ../MacOS/$1/AppleDiagnostics.dmg       $URL/AppleDiagnostics.dmg
+wget -O ~/MacOS/$1/AppleDiagnostics.dmg       $URL/AppleDiagnostics.dmg
 
 echo "Downloading AppleDiagnostics.chunklist..."
-wget -O ../MacOS/$1/AppleDiagnostics.chunklist $URL/AppleDiagnostics.chunklist
+wget -O ~/MacOS/$1/AppleDiagnostics.chunklist $URL/AppleDiagnostics.chunklist
 
 echo "Downloading BaseSystem.dmg..."
-wget -O ../MacOS/$1/BaseSystem.dmg             $URL/BaseSystem.dmg
+wget -O ~/MacOS/$1/BaseSystem.dmg             $URL/BaseSystem.dmg
 
 echo "Downloading BaseSystem.chunklist..."
-wget -O ../MacOS/$1/BaseSystem.chunklist       $URL/BaseSystem.chunklist
+wget -O ~/MacOS/$1/BaseSystem.chunklist       $URL/BaseSystem.chunklist
 
 echo "Downloading InstallInfo.plist..."
-wget -O ../MacOS/$1/InstallInfo.plist          $URL/InstallInfo.plist
+wget -O ~/MacOS/$1/InstallInfo.plist          $URL/InstallInfo.plist
 
 echo "Downloading InstallESD.dmg..."
-wget -O ../MacOS/$1/InstallESD.dmg             $URL/InstallESDDmg.pkg
+wget -O ~/MacOS/$1/InstallESD.dmg             $URL/InstallESDDmg.pkg
 
 echo "Fixing InstallInfo.plist..."
-sed -i '30,33 d'                                                  ../MacOS/$1/InstallInfo.plist
-sed -i 's/InstallESDDmg.pkg/InstallESD.dmg/g'                     ../MacOS/$1/InstallInfo.plist
-sed -i 's/com.apple.pkg.InstallESDDmg/com.apple.dmg.InstallESD/g' ../MacOS/$1/InstallInfo.plist
+sed -i '30,33 d'                                                  ~/MacOS/$1/InstallInfo.plist
+sed -i 's/InstallESDDmg.pkg/InstallESD.dmg/g'                     ~/MacOS/$1/InstallInfo.plist
+sed -i 's/com.apple.pkg.InstallESDDmg/com.apple.dmg.InstallESD/g' ~/MacOS/$1/InstallInfo.plist
 
 echo "Done"
