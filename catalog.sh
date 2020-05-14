9a72d5ac5fbd49e6264ee084a9047a237d6f74d5
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 15:44:40 +0100

    Update catalog.sh

--- catalog.sh
+++ catalog.sh
@@ -9,12 +9,12 @@ mkdir -p ~/MacOS/Catalog
 cd       ~/MacOS/Catalog
 
 wget -q -O catalog.gz https://swscan.apple.com/content/catalogs/others/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
-zgrep "InstallInfo.plist" catalog.gz | sed 's/[[:blank:]]//g' | sed 's/<string>//g' | sed 's/<\/string>//g' > catalog.txt
+zgrep "InstallInfo.plist" catalog.gz | sed 's/[[:space:]]//g' | sed 's/<string>//g' | sed 's/<\/string>//g' > catalog.txt
 
 touch InstallInfo.plist
 wget -q -i catalog.txt
 
-for i in 1 2 3 4 5 6 7 8 9; do cat InstallInfo.plist.$i | grep -m 1 "10." | sed 's/[[:blank:]]//g' | sed 's/<string>//g' | sed 's/<\/string>//g' >> version.txt
+for i in 1 2 3 4 5 6 7 8 9; do cat InstallInfo.plist.$i | grep -m 1 "10." | sed 's/[[:space:]]//g' | sed 's/<string>//g' | sed 's/<\/string>//g' >> version.txt
 done
 
 n=1; while read -r catalog; do catalog_array[n]=$catalog; ((n++)); done < catalog.txt
