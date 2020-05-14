bc4e36fcffd7ecfb1833448cef8e8f4376e56c99
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 12:45:32 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,16 +3,14 @@
 USER1=""
 USER2=""
 
-rm -f  Clover.User/$user1/EFI/BOOT/.empty
-rm -rf Clover.User/$user1/EFI/CLOVER/themes/*
-cp -rf Clover.Config/Themes/Clovy              Clover.User/$user1/EFI/CLOVER/themes/
-cp -f  Clover.Config/DSDT/$user1.aml           Clover.User/$user1/EFI/CLOVER/ACPI/patched/DSDT.aml
-cp -f  Clover/$clover/EFI/BOOT/BOOTX64.efi     Clover.User/$user1/EFI/BOOT/BOOTX64.efi
-cp -f  Clover/$clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$user1/EFI/CLOVER/CLOVERX64.efi
+rm -f  Clover.User/$USER1/EFI/BOOT/.empty
+rm -rf Clover.User/$USER1/EFI/CLOVER/themes/*
+cp -f  Clover.Config/DSDT/$USER1.aml   Clover.User/$USER1/EFI/CLOVER/ACPI/patched/DSDT.aml
+cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER1/EFI/BOOT/BOOTX64.efi
+cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER1/EFI/CLOVER/CLOVERX64.efi
 
-rm -f  Clover.User/$user2/EFI/BOOT/.empty
-rm -rf Clover.User/$user2/EFI/CLOVER/themes/*
-cp -rf Clover.Config/Themes/Clovy              Clover.User/$user2/EFI/CLOVER/themes/
-cp -f  Clover.Config/DSDT/$user2.aml           Clover.User/$user2/EFI/CLOVER/ACPI/patched/DSDT.aml
-cp -f  Clover/$clover/EFI/BOOT/BOOTX64.efi     Clover.User/$user2/EFI/BOOT/BOOTX64.efi
-cp -f  Clover/$clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$user2/EFI/CLOVER/CLOVERX64.efi
+rm -f  Clover.User/$USER2/EFI/BOOT/.empty
+rm -rf Clover.User/$USER2/EFI/CLOVER/themes/*
+cp -f  Clover.Config/DSDT/$USER2.aml   Clover.User/$USER2/EFI/CLOVER/ACPI/patched/DSDT.aml
+cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER2/EFI/BOOT/BOOTX64.efi
+cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER2/EFI/CLOVER/CLOVERX64.efi
