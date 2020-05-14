964530dbc2a4f9102a1ae274436e4ddf40f70a57
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 15:53:26 +0100

    Update catalog.sh

--- catalog.sh
+++ catalog.sh
@@ -8,12 +8,15 @@ fi
 mkdir -p ~/MacOS/Catalog
 cd       ~/MacOS/Catalog
 
+echo "Downloading Catalog..."
 wget -q -O catalog.gz https://swscan.apple.com/content/catalogs/others/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
 zgrep "InstallInfo.plist" catalog.gz | sed 's/[[:space:]]//g' | sed 's/<string>//g' | sed 's/<\/string>//g' > catalog.txt
 
+echo "Downloading Versions..."
 touch InstallInfo.plist
 wget -q -i catalog.txt
 
+echo "Processing..."
 for i in 1 2 3 4 5 6 7 8 9; do cat InstallInfo.plist.$i | grep -m 1 "10." | sed 's/[[:space:]]//g' | sed 's/<string>//g' | sed 's/<\/string>//g' >> version.txt
 done
 
@@ -23,7 +26,11 @@ n=1; while read -r version; do version_array[n]=$version; ((n++)); done < versio
 for i in 1 2 3 4 5 6 7 8 9; do echo ${version_array[i]} 'URL="'${catalog_array[i]} | sed 's/\/InstallInfo.plist/"/g' >> urls.txt
 done
 
+echo "Searching..."
 cat urls.txt | grep $1
 
+echo "Cleaning Up..."
 cd     ~/MacUSB
 rm -rf ~/MacOS/Catalog
+
+echo "Done"
