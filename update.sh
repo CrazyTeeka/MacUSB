56eac8d6d57cdd540df4a5089a2843b0be558b60
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 13:01:53 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,9 +4,11 @@ USER1="CLOVER.SERIES.CHIPSET"
 USER2="CLOVER.SERIES.100.200.300"
 
 echo "Updating Clover..."
-cp -f Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER1/EFI/BOOT/
-cp -f Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER1/EFI/CLOVER/
-cp -f Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER2/EFI/BOOT/
-cp -f Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER2/EFI/CLOVER/
+cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER1/EFI/BOOT/
+cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER1/EFI/CLOVER/
+cp -rf Clover/EFI/CLOVER/tools         Clover.User/$USER1/EFI/CLOVER/
+cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER2/EFI/BOOT/
+cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER2/EFI/CLOVER/
+cp -rf Clover/EFI/CLOVER/tools         Clover.User/$USER2/EFI/CLOVER/
 
 echo "Done"
