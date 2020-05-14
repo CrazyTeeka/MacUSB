356e659e41891a17cf29bd0ec6234501ed6b24a4
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 12:54:57 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-USER1=""
-USER2=""
+USER1="CLOVER.SERIES.CHIPSET"
+USER2="CLOVER.SERIES.100.200.300"
 
+echo "Updating Clover..."
 cp -f Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER1/EFI/BOOT/
 cp -f Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER1/EFI/CLOVER/
 cp -f Clover/EFI/BOOT/BOOTX64.efi     Clover.User/$USER2/EFI/BOOT/
 cp -f Clover/EFI/CLOVER/CLOVERX64.efi Clover.User/$USER2/EFI/CLOVER/
+
+echo "Done"
