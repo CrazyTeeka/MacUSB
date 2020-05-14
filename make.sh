45df72410d8c9057cb0fea98a5a3563e139fa72a
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-14 14:18:58 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -18,38 +18,52 @@ P1=$USB"1" # Partition 1
 P2=$USB"2" # Partition 2
 P3=$USB"3" # Partition 3
 
+echo "Updating Clover..."
 sh update.sh
 
+echo "Unmounting..."
 sudo umount $P1
 sudo umount $P2
 sudo umount $P3
 
+echo "Formatting..."
 sudo gdisk $USB < macOS.gdisk
 sudo mkfs.fat -F 32 -n EFI -v $P1
 sudo mkfs.hfsplus -v BaseSystem $P2
 sudo mkfs.hfsplus -v macOS $P3
 
+echo "Writing Base System..."
 sudo dmg2img -i ../MacOS/$VER/BaseSystem.dmg -p 4 -o $P2
 
+echo "Preparing..."
 sudo mkdir -p /media/$USER/EFI
 sudo mkdir -p /media/$USER/macOS
+sudo rm -rf   /media/$USER/EFI/*
+sudo rm -rf   /media/$USER/macOS/*
 
-sudo rm -rf /media/$USER/EFI/*
-sudo rm -rf /media/$USER/macOS/*
-
+echo "Mounting..."
 sudo mount $P1 /media/$USER/EFI
 sudo mount $P3 /media/$USER/macOS
 
+echo "Copying EFI..."
 sudo cp -rf $EFI/EFI /media/$USER/EFI/
 sudo cp -rf $EFI/EFI /media/$USER/macOS/
 
+echo "Copying MacOS..."
 sudo mkdir -p /media/$USER/macOS/SharedSupport
 sudo cp -rf ../MacOS/$VER/* /media/$USER/macOS/SharedSupport/
 
+echo "Copying Scripts..."
 sudo cp -rf Scripts  /media/$USER/macOS/
+
+echo "Copying Tools..."
 sudo cp -rf Tools    /media/$USER/macOS/
 
+echo "Cleaning Up..."
 sudo chown -R $USER:$USER /media/$USER/macOS/*
 sudo chmod +x /media/$USER/macOS/Scripts/*
 
+echo "Ejecting..."
 sudo eject $USB
+
+echo "Done"
