0c2578c3073c59289cc1e185c2fcb5c465fe2399
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-16 17:12:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ CLOVER="5117"
 
 USER1="CLOVER.SERIES.CHIPSET"
 USER2="CLOVER.SERIES.100.200.300"
+USER3="CLOVER.NOTEBOOKS.SERIES.CHIPSET"
+USER4="CLOVER.NOTEBOOKS.100.200.300"
 
 echo "Removing Folders..."
 rm -rf Clover
@@ -21,6 +23,8 @@ mkdir -p Clover.Config/DSDT
 mkdir -p Clover.Config/THEMES
 mkdir -p Clover.Folder/$USER1
 mkdir -p Clover.Folder/$USER2
+mkdir -p Clover.Folder/$USER3
+mkdir -p Clover.Folder/$USER4
 mkdir -p Scripts
 mkdir -p Tools
 mkdir -p Temp/$USER1
@@ -31,6 +35,8 @@ wget -q -P Temp/                 https://github.com/CloverHackyColor/CloverBootl
 wget -q -P Temp/                 https://github.com/CrazyTeeka/Clover-Themes/raw/master/Clovy.zip
 wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER1.zip
 wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER2.zip
+wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER3.zip
+wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER4.zip
 wget -q -P Clover.Config/CONFIG/ https://github.com/CrazyTeeka/Clover-Config/raw/master/CLOVER.SERIES.CHIPSET.plist
 wget -q -P Clover.Config/CONFIG/ https://github.com/CrazyTeeka/Clover-Config/raw/master/CLOVER.SERIES.100.200.300.plist
 wget -q -P Clover.Config/DSDT/   https://github.com/CrazyTeeka/Clover-DSDT/raw/master/MAXIMUS-VII-HERO-ASUS-3503.aml
@@ -44,6 +50,8 @@ unzip -o -qq Temp/Clovy.zip            -d Clover.Config/THEMES/
 unzip -o -qq Temp/Scripts.zip          -d Temp/
 unzip -o -qq Temp/$USER1.zip           -d Temp/$USER1/
 unzip -o -qq Temp/$USER2.zip           -d Temp/$USER2/
+unzip -o -qq Temp/$USER3.zip           -d Temp/$USER3/
+unzip -o -qq Temp/$USER4.zip           -d Temp/$USER4/
 
 echo "Copying Files..."
 cp -f  Temp/CloverV2/EFI/BOOT/BOOTX64.efi                   Clover/EFI/BOOT/
@@ -52,6 +60,8 @@ cp -rf Temp/CloverV2/EFI/CLOVER/drivers                     Clover/EFI/CLOVER/
 cp -rf Temp/CloverV2/EFI/CLOVER/tools                       Clover/EFI/CLOVER/
 cp -rf Temp/$USER1/EFI                                      Clover.Folder/$USER1/
 cp -rf Temp/$USER2/EFI                                      Clover.Folder/$USER2/
+cp -rf Temp/$USER3/EFI                                      Clover.Folder/$USER3/
+cp -rf Temp/$USER4/EFI                                      Clover.Folder/$USER4/
 cp -f  Clover.Config/CONFIG/CLOVER.SERIES.CHIPSET.plist     Clover.Folder/$USER1/EFI/CLOVER/config.plist
 cp -f  Clover.Config/CONFIG/CLOVER.SERIES.100.200.300.plist Clover.Folder/$USER2/EFI/CLOVER/config.plist
 #cp -f  Clover.Config/DSDT/MAXIMUS-VII-HERO-ASUS-3503.aml    Clover.Folder/$USER1/EFI/CLOVER/ACPI/patched/DSDT.aml
