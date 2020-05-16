8da2bf455ef8ef4d8c4988c2ee5c57d2ed774794
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-16 17:24:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,8 +37,8 @@ wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$US
 wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER2.zip
 wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER3.zip
 wget -q -P Temp/                 https://www.olarila.com/files/Clover.Folder/$USER4.zip
-wget -q -P Clover.Config/CONFIG/ https://github.com/CrazyTeeka/Clover-Config/raw/master/CLOVER.SERIES.CHIPSET.plist
-wget -q -P Clover.Config/CONFIG/ https://github.com/CrazyTeeka/Clover-Config/raw/master/CLOVER.SERIES.100.200.300.plist
+wget -q -P Clover.Config/CONFIG/ https://github.com/CrazyTeeka/Clover-Config/raw/master/SERIES.CHIPSET.plist
+wget -q -P Clover.Config/CONFIG/ https://github.com/CrazyTeeka/Clover-Config/raw/master/SERIES.100.200.300.plist
 wget -q -P Clover.Config/DSDT/   https://github.com/CrazyTeeka/Clover-DSDT/raw/master/MAXIMUS-VII-HERO-ASUS-3503.aml
 wget -q -P Clover.Config/DSDT/   https://github.com/CrazyTeeka/Clover-DSDT/raw/master/MAXIMUS-VIII-GENE-ASUS.aml
 wget -q -O Temp/Scripts.zip      https://github.com/CrazyTeeka/MacUSB-Scripts/archive/master.zip
