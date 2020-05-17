d6a281d37d07f834154af3edffcd49ebecb4f0af
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-05-17 19:07:08 +0100

    Add files via upload

--- clover-update.sh
+++ clover-update.sh
@@ -20,3 +20,15 @@ cp -rf Clover/EFI/CLOVER/tools         Clover.Folder/$USER3/EFI/CLOVER/
 cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.Folder/$USER4/EFI/BOOT/
 cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.Folder/$USER4/EFI/CLOVER/
 cp -rf Clover/EFI/CLOVER/tools         Clover.Folder/$USER4/EFI/CLOVER/
+
+if [ -d "Clover.Custom/James" ]; then
+   cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.Custom/James/EFI/BOOT/
+   cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.Custom/James/EFI/CLOVER/
+   cp -rf Clover/EFI/CLOVER/tools         Clover.Custom/James/EFI/CLOVER/
+fi
+
+if [ -d "Clover.Custom/Frank" ]; then
+   cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.Custom/Frank/EFI/BOOT/
+   cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.Custom/Frank/EFI/CLOVER/
+   cp -rf Clover/EFI/CLOVER/tools         Clover.Custom/Frank/EFI/CLOVER/
+fi
