6da363994c18f0e1524dd3757c56ca08d040b87e
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-06-20 17:25:25 +0100

    Update run.sh

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -3,51 +3,25 @@
 SOURCE="/Volumes/USB/Mac"
 TARGET="/Volumes/USB/Mac"
 
-if [ -t 1 ]; then
-  tty_escape() { printf "\033[%sm" "$1"; }
-else
-  tty_escape() { :; }
-fi
-
-tty_mkbold() { tty_escape "1;$1"; }
-
-tty_blue="$(tty_mkbold 34)"
-tty_bold="$(tty_mkbold 39)"
-tty_reset="$(tty_escape 0)"
-
 if [ "$1" = "backup" ]; then
   mkdir -p $TARGET/${(C)USER}/Applications
   mkdir -p $TARGET/${(C)USER}/Firefox
   mkdir -p $TARGET/${(C)USER}/Home
-  echo "${tty_blue}==>${tty_bold} Copying Applications...${tty_reset}"
   rsync -a /Applications/* $TARGET/${(C)USER}/Applications/
+  rsync -a $HOME/Documents $TARGET/${(C)USER}/Home/
+  rsync -a $HOME/Downloads $TARGET/${(C)USER}/Home/
+  rsync -a $HOME/Movies    $TARGET/${(C)USER}/Home/
+  rsync -a $HOME/Music     $TARGET/${(C)USER}/Home/
+  rsync -a $HOME/Pictures  $TARGET/${(C)USER}/Home/
   rm -rf $TARGET/${(C)USER}/Applications/Safari.app
   rm -rf $TARGET/${(C)USER}/Applications/Utilities
-  echo "${tty_blue}==>${tty_bold} Copying Documents...${tty_reset}"
-  rsync -a $HOME/Documents $TARGET/${(C)USER}/Home/
-  echo "${tty_blue}==>${tty_bold} Copying Downloads...${tty_reset}"
-  rsync -a $HOME/Downloads $TARGET/${(C)USER}/Home/
-  echo "${tty_blue}==>${tty_bold} Copying Movies...${tty_reset}"
-  rsync -a $HOME/Movies    $TARGET/${(C)USER}/Home/
-  echo "${tty_blue}==>${tty_bold} Copying Music...${tty_reset}"
-  rsync -a $HOME/Music     $TARGET/${(C)USER}/Home/
-  echo "${tty_blue}==>${tty_bold} Copying Pictures...${tty_reset}"
-  rsync -a $HOME/Pictures  $TARGET/${(C)USER}/Home/
-  echo "${tty_blue}==>${tty_bold} Done${tty_reset}"
 elif [ "$1" = "restore" ]; then
-  echo "${tty_blue}==>${tty_bold} Restoring Applications...${tty_reset}"
   rsync -a $SOURCE/${(C)USER}/Applications/* /Applications/
-  echo "${tty_blue}==>${tty_bold} Restoring Documents...${tty_reset}"
   rsync -a $SOURCE/${(C)USER}/Home/Documents $HOME/
-  echo "${tty_blue}==>${tty_bold} Restoring Downloads...${tty_reset}"
   rsync -a $SOURCE/${(C)USER}/Home/Downloads $HOME/
-  echo "${tty_blue}==>${tty_bold} Restoring Movies...${tty_reset}"
   rsync -a $SOURCE/${(C)USER}/Home/Movies    $HOME/
-  echo "${tty_blue}==>${tty_bold} Restoring Music...${tty_reset}"
   rsync -a $SOURCE/${(C)USER}/Home/Music     $HOME/
-  echo "${tty_blue}==>${tty_bold} Restoring Pictures...${tty_reset}"
   rsync -a $SOURCE/${(C)USER}/Home/Pictures  $HOME/
-  echo "${tty_blue}==>${tty_bold} Done${tty_reset}"
 elif [ "$1" = "copy-efi" ]; then
   sudo diskutil mount /dev/disk0s1 >/dev/null 2>/dev/null
   sudo rm -rf /Volumes/EFI/EFI
