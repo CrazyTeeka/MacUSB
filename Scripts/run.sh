cd0c90b24d9f5b835d52967777f46635a4f2670d
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-06-30 15:30:52 +0100

    Add files via upload

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -5,6 +5,8 @@ TARGET="/Volumes/USB/Mac"
 
 if [ -z "$1" ]; then
   echo "Usage: run.sh backup"
+  echo "       run.sh backup-apps"
+  echo "       run.sh backup-firefox"
   echo "       run.sh restore"
   echo "       run.sh copy-clover"
   echo "       run.sh setup"
@@ -16,19 +18,25 @@ if [ -z "$1" ]; then
 fi
 
 if [ "$1" = "backup" ]; then
-  mkdir -p $TARGET/${(C)USER}/Applications
-  mkdir -p $TARGET/${(C)USER}/Firefox
   mkdir -p $TARGET/${(C)USER}/Home
-  rsync -a /Applications/* $TARGET/${(C)USER}/Applications/
   rsync -a $HOME/Documents $TARGET/${(C)USER}/Home/
   rsync -a $HOME/Downloads $TARGET/${(C)USER}/Home/
   rsync -a $HOME/Movies    $TARGET/${(C)USER}/Home/
   rsync -a $HOME/Music     $TARGET/${(C)USER}/Home/
   rsync -a $HOME/Pictures  $TARGET/${(C)USER}/Home/
-  rm -rf $TARGET/${(C)USER}/Applications/Safari.app
-  rm -rf $TARGET/${(C)USER}/Applications/Utilities
+elif [ "$1" = "backup-apps" ]; then
+  mkdir -p $TARGET/${(C)USER}/Applications
+  rsync -a /Applications/* $TARGET/${(C)USER}/Applications/
+elif [ "$1" = "backup-firefox" ]; then
+  mkdir -p $TARGET/${(C)USER}/Library/Application\ Support/Firefox
+  mkdir -p $TARGET/${(C)USER}/Library/Caches/Firefox
+  mkdir -p $TARGET/${(C)USER}/Library/Preferences
+  mkdir -p $TARGET/${(C)USER}/Library/Saved\ Application\ State
+  rsync -a $HOME/Library/Application\ Support/Firefox                             $TARGET/${(C)USER}/Library/Application\ Support/
+  rsync -a $HOME/Library/Caches/Firefox                                           $TARGET/${(C)USER}/Library/Caches/
+  rsync -a $HOME/Library/Preferences/org.mozilla.firefox.plist                    $TARGET/${(C)USER}/Library/Preferences/
+  rsync -a $HOME/Library/Saved\ Application\ State/org.mozilla.firefox.savedState $TARGET/${(C)USER}/Library/Saved\ Application\ State/
 elif [ "$1" = "restore" ]; then
-  rsync -a $SOURCE/${(C)USER}/Applications/* /Applications/
   rsync -a $SOURCE/${(C)USER}/Home/Documents $HOME/
   rsync -a $SOURCE/${(C)USER}/Home/Downloads $HOME/
   rsync -a $SOURCE/${(C)USER}/Home/Movies    $HOME/
