d42a830f057f9ddd526089b3b865ef9fc4992688
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-07-02 20:03:50 +0100

    Add files via upload

--- clover-update.sh
+++ clover-update.sh
@@ -36,4 +36,9 @@ if [ -d "Clover.Custom/Frank" ]; then
 cp -f  Clover/EFI/BOOT/BOOTX64.efi     Clover.Custom/Frank/EFI/BOOT/
 cp -f  Clover/EFI/CLOVER/CLOVERX64.efi Clover.Custom/Frank/EFI/CLOVER/
 cp -rf Clover/EFI/CLOVER/tools         Clover.Custom/Frank/EFI/CLOVER/
+cp -rf Kexts/AirportBrcmFixup.kext     Clover.Custom/Frank/EFI/CLOVER/kexts/Other/
+cp -rf Kexts/AppleALC.kext             Clover.Custom/Frank/EFI/CLOVER/kexts/Other/
+cp -rf Kexts/Lilu.kext                 Clover.Custom/Frank/EFI/CLOVER/kexts/Other/
+cp -rf Kexts/VirtualSMC.kext           Clover.Custom/Frank/EFI/CLOVER/kexts/Other/
+cp -rf Kexts/WhateverGreen.kext        Clover.Custom/Frank/EFI/CLOVER/kexts/Other/
 fi
