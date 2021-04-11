5e5f32c9405ad584827d4319dc2c628be76fb1b2
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2021-04-11 15:21:06 +0100

    Add files via upload

--- download.sh
+++ download.sh
@@ -3,15 +3,20 @@
 TARGET="$HOME/MacOS"
 
 if [ -z "$1" ]; then
-  echo "Usage: download.sh <version>"
+  echo "Usage: download.sh Catalina"
+  echo "       download.sh Mojave"
+  echo "       download.sh HighSierra"
   exit 0
 fi
 
-if [ "$1" = "10.15.7" ]; then URL="http://swcdn.apple.com/content/downloads/26/37/001-68446/r1dbqtmf3mtpikjnd04cq31p4jk91dceh8"
+if   [ "$1" = "Catalina"   ]; then URL="http://swcdn.apple.com/content/downloads/26/37/001-68446/r1dbqtmf3mtpikjnd04cq31p4jk91dceh8"
+elif [ "$1" = "Mojave"     ]; then URL="http://swcdn.apple.com/content/downloads/17/32/061-26589-A_8GJTCGY9PC/25fhcu905eta7wau7aoafu8rvdm7k1j4el"
+elif [ "$1" = "HighSierra" ]; then URL="http://swcdn.apple.com/content/downloads/06/50/041-91758-A_M8T44LH2AW/b5r4og05fhbgatve4agwy4kgkzv07mdid9"
 else exit 0
 fi
 
-mkdir -p "$TARGET/$1"
+rm    -rf "$TARGET/$1"
+mkdir -p  "$TARGET/$1"
 
 wget -q -P "$TARGET/$1/" "$URL/AppleDiagnostics.dmg"
 wget -q -P "$TARGET/$1/" "$URL/AppleDiagnostics.chunklist"
