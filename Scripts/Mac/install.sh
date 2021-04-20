ebcedbd5696001348b28d49d03c048c8955d17c3
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2021-04-20 22:30:51 +0100

    Update install.sh

--- Scripts/Mac/install.sh
+++ Scripts/Mac/install.sh
@@ -2,11 +2,8 @@
 
 MACOS="Catalina"
 
-if [ -z "$1" ]; then
-  cp -R /Install\ macOS\ $MACOS.app /Volumes/Macintosh\ HD/
-  cp -R /Volumes/macOS/SharedSupport /Volumes/Macintosh\ HD/Install\ macOS\ $MACOS.app/Contents/
-  /Volumes/Macintosh\ HD/Install\ macOS\ $MACOS.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD
-elif [ "$1" = "upgrade" ]; then
-  cp -R /Volumes/macOS\ Base\ System/Install\ macOS\ $MACOS.app $HOME/Desktop/
-  cp -R /Volumes/macOS/SharedSupport $HOME/Desktop/Install\ macOS\ $MACOS.app/Contents/
+if [ $MACOS = "Catalina" ]; then
+  cp -R /Install\ macOS\ Catalina.app /Volumes/Macintosh\ HD/
+  cp -R /Volumes/macOS/SharedSupport /Volumes/Macintosh\ HD/Install\ macOS\ Catalina.app/Contents/
+  /Volumes/Macintosh\ HD/Install\ macOS\ Catalina.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD
 fi
