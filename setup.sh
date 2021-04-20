e293a981f9ea579fb5caaf0d9832e30ba58eaff4
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2021-04-20 22:10:03 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-OpenCore="0.6.8"
+OpenCoreVersion="0.6.8"
 
-FILES="https://github.com/CrazyTeeka/MacUSB/raw/master/Files"
+DSDT="https://github.com/CrazyTeeka/MacUSB/raw/master/DSDT"
 KEXTS="https://github.com/CrazyTeeka/MacUSB/raw/master/Kexts"
+OPENCORE="https://github.com/CrazyTeeka/MacUSB/raw/master/OpenCore"
 SCRIPTS="https://github.com/CrazyTeeka/MacUSB/raw/master/Scripts"
+TOOLS="https://github.com/CrazyTeeka/MacUSB/raw/master/Tools"
 
-echo "OpenCore: $OpenCore"
+echo "OpenCore: $OpenCoreVersion"
 echo ""
 
 echo "Preparing..."
@@ -36,22 +38,14 @@ mkdir -p OpenCore/Update
 
 echo "Downloading Files..."
 
-wget -q -P DSDT/ $FILES/DSDT-Asus-Maximus-VII-Hero-3503.aml
-wget -q -P DSDT/ $FILES/DSDT-Asus-Maximus-VIII-Gene-1902.aml
+wget -q -P Temp/ $OPENCORE/OpenCore-$OpenCoreVersion-RELEASE.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-9.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-200.zip
+wget -q -P Docs/ $OPENCORE/OpenCore-Configuration.pdf
+wget -q -P Tools/ $OPENCORE/OpenCore-Configurator.zip
 
-wget -q -P Temp/ $FILES/OpenCore-$OpenCore-RELEASE.zip
-wget -q -P Temp/ $FILES/OpenCore-Series-9.zip
-wget -q -P Temp/ $FILES/OpenCore-Series-200.zip
-
-wget -q -P Docs/ $FILES/OpenCore-Configuration.pdf
-wget -q -P Tools/ $FILES/OpenCore-Configurator.zip
-
-wget -q -P Temp/ $FILES/Tools-AppCleaner.zip
-wget -q -P Temp/ $FILES/Tools-ESPMounterPro.zip
-wget -q -P Temp/ $FILES/Tools-OCKexts.zip
-wget -q -P Temp/ $FILES/Tools-PlistEditPro.zip
-wget -q -P Temp/ $FILES/Tools-Releases.zip
-wget -q -P Temp/ $FILES/Tools-RunMe.zip
+wget -q -P DSDT/ $DSDT/Asus-RoG-Maximus-VII-Hero-3503.aml
+wget -q -P DSDT/ $DSDT/Asus-RoG-Maximus-VIII-Gene-1902.aml
 
 wget -q -P Temp/ $KEXTS/AirportBrcmFixup-2.1.2-RELEASE.zip
 wget -q -P Temp/ $KEXTS/AppleALC-1.5.9-RELEASE.zip
@@ -66,9 +60,15 @@ wget -q -P Temp/ $KEXTS/WhateverGreen-1.4.9-RELEASE.zip
 wget -q -P Scripts/ $SCRIPTS/install.sh
 wget -q -P Scripts/ $SCRIPTS/run.sh
 
+wget -q -P Tools/ $TOOLS/App-Cleaner.zip
+wget -q -P Tools/ $TOOLS/ESP-Mounter-Pro.zip
+wget -q -P Tools/ $TOOLS/Kexts-Compiler.zip
+wget -q -P Tools/ $TOOLS/Kexts-Releases.zip
+wget -q -P Tools/ $TOOLS/Run-Me.zip
+
 echo "Unpacking Files..."
 
-unzip -o -qq Temp/OpenCore-$OpenCore-RELEASE.zip -d OpenCore/Update/
+unzip -o -qq Temp/OpenCore-$OpenCoreVersion-RELEASE.zip -d OpenCore/Update/
 unzip -o -qq Temp/OpenCore-Series-9.zip -d OpenCore/Series-9/
 unzip -o -qq Temp/OpenCore-Series-200.zip -d OpenCore/Series-100/
 unzip -o -qq Temp/OpenCore-Series-200.zip -d OpenCore/Series-200/
@@ -83,43 +83,22 @@ unzip -o -qq Temp/RestrictEvents-1.0.0-RELEASE.zip -d Kexts/
 unzip -o -qq Temp/VirtualSMC-1.2.2-RELEASE.zip -d Kexts/
 unzip -o -qq Temp/WhateverGreen-1.4.9-RELEASE.zip -d Kexts/
 
+unzip -o -qq Tools/OpenCore-Configurator.zip -d Tools/
+unzip -o -qq Tools/App-Cleaner.zip -d Tools/
+unzip -o -qq Tools/ESP-Mounter-Pro.zip -d Tools/
+unzip -o -qq Tools/Kexts-Compiler.zip -d Tools/
+unzip -o -qq Tools/Kexts-Releases.zip -d Tools/
+unzip -o -qq Tools/Run-Me.zip -d Tools/
+
+rm -rf Tools/*.zip
+
 echo "Organising Files..."
 
 cp -rf OpenCore/Update/X64/EFI OpenCore/Update/
-
-cp -f Temp/Tools-AppCleaner.zip Tools/AppCleaner.zip
-cp -f Temp/Tools-ESPMounterPro.zip Tools/ESPMounterPro.zip
-cp -f Temp/Tools-OCKexts.zip Tools/OCKexts.zip
-cp -f Temp/Tools-PlistEditPro.zip Tools/PlistEditPro.zip
-cp -f Temp/Tools-Releases.zip Tools/Releases.zip
-cp -f Temp/Tools-RunMe.zip Tools/RunMe.zip
-
-cp -rf Kexts/AtherosE2200Ethernet-V2.2.2/Release/AtherosE2200Ethernet.kext Kexts/
-rm -rf Kexts/AtherosE2200Ethernet-V2.2.2
-
-mkdir -p Kexts/Kexts/VirtualSMC.kext/Contents/Plugins
-
-cp -rf Kexts/Kexts/SMCLightSensor.kext Kexts/Kexts/VirtualSMC.kext/Contents/Plugins/
-cp -rf Kexts/Kexts/SMCProcessor.kext Kexts/Kexts/VirtualSMC.kext/Contents/Plugins/
-cp -rf Kexts/Kexts/VirtualSMC.kext Kexts/
-rm -rf Kexts/Kexts
-
-cp -rf Kexts/RealtekRTL8111-V2.4.0/Release/RealtekRTL8111.kext Kexts/
-rm -rf Kexts/RealtekRTL8111-V2.4.0
-
-rm -rf Kexts/IntelSnowMausi.kext
-rm -rf Kexts/*.dSYM
-rm -rf Kexts/dSYM
-rm -rf Kexts/__MACOSX
-rm -rf Kexts/Tools
-rm -rf Kexts/WhateverName.app
-rm -f  Kexts/alc-verb
-
 rm -rf OpenCore/Update/Docs
 rm -rf OpenCore/Update/IA32
 rm -rf OpenCore/Update/Utilities
 rm -rf OpenCore/Update/X64
-
 rm -rf OpenCore/Update/EFI/OC/ACPI
 rm -rf OpenCore/Update/EFI/OC/Kexts
 rm -rf OpenCore/Update/EFI/OC/Resources
@@ -132,6 +111,23 @@ rm -f  OpenCore/Update/EFI/OC/Drivers/Ps2KeyboardDxe.efi
 rm -f  OpenCore/Update/EFI/OC/Drivers/Ps2MouseDxe.efi
 rm -f  OpenCore/Update/EFI/OC/Drivers/UsbMouseDxe.efi
 rm -f  OpenCore/Update/EFI/OC/Drivers/XhciDxe.efi
+
+mkdir -p Kexts/Kexts/VirtualSMC.kext/Contents/Plugins
+cp -rf Kexts/AtherosE2200Ethernet-V2.2.2/Release/AtherosE2200Ethernet.kext Kexts/
+rm -rf Kexts/AtherosE2200Ethernet-V2.2.2
+cp -rf Kexts/Kexts/SMCLightSensor.kext Kexts/Kexts/VirtualSMC.kext/Contents/Plugins/
+cp -rf Kexts/Kexts/SMCProcessor.kext Kexts/Kexts/VirtualSMC.kext/Contents/Plugins/
+cp -rf Kexts/Kexts/VirtualSMC.kext Kexts/
+rm -rf Kexts/Kexts
+cp -rf Kexts/RealtekRTL8111-V2.4.0/Release/RealtekRTL8111.kext Kexts/
+rm -rf Kexts/RealtekRTL8111-V2.4.0
+rm -rf Kexts/IntelSnowMausi.kext
+rm -rf Kexts/*.dSYM
+rm -rf Kexts/dSYM
+rm -rf Kexts/__MACOSX
+rm -rf Kexts/Tools
+rm -rf Kexts/WhateverName.app
+rm -f  Kexts/alc-verb
 
 rm -rf Temp
 echo "Done"
