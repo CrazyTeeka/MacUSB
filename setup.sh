3d027338001eb630cc0fa8904218c64dd192f1a8
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2021-08-31 02:02:05 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ OpenCoreVersion="0.7.2"
 
 DSDT="https://github.com/CrazyTeeka/MacUSB/raw/master/DSDT"
 KEXTS="https://github.com/CrazyTeeka/MacUSB/raw/master/Kexts"
+CLOVER="https://github.com/CrazyTeeka/MacUSB/raw/master/Clover"
 OPENCORE="https://github.com/CrazyTeeka/MacUSB/raw/master/OpenCore"
 SCRIPTS="https://github.com/CrazyTeeka/MacUSB/raw/master/Scripts"
 TOOLS="https://github.com/CrazyTeeka/MacUSB/raw/master/Tools"
 
 echo "Preparing..."
 
+mkdir -p Clover
 mkdir -p OpenCore
 mkdir -p DSDT
 mkdir -p Kexts
@@ -18,6 +20,7 @@ mkdir -p Temp
 mkdir -p Tools
 mkdir -p Uploads
 
+rm -rf Clover/*
 rm -rf OpenCore/*
 rm -rf DSDT/*
 rm -rf Kexts/*
@@ -26,20 +29,46 @@ rm -rf Temp/*
 rm -rf Tools/*
 rm -rf Uploads/*
 
+mkdir -p Clover/Series-5
+mkdir -p Clover/Series-6
+mkdir -p Clover/Series-7
+mkdir -p Clover/Series-8
+mkdir -p Clover/Series-9
+
+mkdir -p OpenCore/Series-6
+mkdir -p OpenCore/Series-7
+mkdir -p OpenCore/Series-8
 mkdir -p OpenCore/Series-9
 mkdir -p OpenCore/Series-100
 mkdir -p OpenCore/Series-200
+mkdir -p OpenCore/Series-300
+mkdir -p OpenCore/Series-400
+mkdir -p OpenCore/Series-500
 mkdir -p OpenCore/Update
+
 mkdir -p Scripts/Linux
 mkdir -p Scripts/Mac
 
 echo "Downloading Files..."
 
+wget -q -P Temp/ $CLOVER/Clover-Series-5.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-6.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-7.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-8.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-9.zip
+
 wget -q -P Temp/ $OPENCORE/OpenCore-$OpenCoreVersion-RELEASE.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-6.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-7.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-8.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-9.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-100.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-200.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-300.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-400.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-500.zip
 
+wget -q -P Tools/ $CLOVER/Clover-Configurator.zip
 wget -q -P Tools/ $OPENCORE/OpenCore-Configurator.zip
 
 #wget -q -P DSDT/ $DSDT/Asus-RoG-Maximus-VII-Hero-3503.aml
@@ -55,7 +84,6 @@ wget -q -P Temp/ $KEXTS/RestrictEvents-1.0.3-RELEASE.zip
 wget -q -P Temp/ $KEXTS/VirtualSMC-1.2.6-RELEASE.zip
 wget -q -P Temp/ $KEXTS/WhateverGreen-1.5.2-RELEASE.zip
 
-wget -q -P Scripts/Linux/ $SCRIPTS/Linux/efi
 wget -q -P Scripts/Linux/ $SCRIPTS/Linux/update
 wget -q -P Scripts/Linux/ $SCRIPTS/Linux/zip
 
@@ -71,10 +99,22 @@ wget -q -P Scripts/Mac/ $SCRIPTS/Mac/trim-enable
 
 echo "Unpacking Files..."
 
+unzip -o -qq Temp/Clover-Series-5.zip -d Clover/Series-5/
+unzip -o -qq Temp/Clover-Series-6.zip -d Clover/Series-6/
+unzip -o -qq Temp/Clover-Series-7.zip -d Clover/Series-7/
+unzip -o -qq Temp/Clover-Series-8.zip -d Clover/Series-8/
+unzip -o -qq Temp/Clover-Series-9.zip -d Clover/Series-9/
+
 unzip -o -qq Temp/OpenCore-$OpenCoreVersion-RELEASE.zip -d OpenCore/Update/
+unzip -o -qq Temp/OpenCore-Series-6.zip -d OpenCore/Series-6/
+unzip -o -qq Temp/OpenCore-Series-7.zip -d OpenCore/Series-7/
+unzip -o -qq Temp/OpenCore-Series-8.zip -d OpenCore/Series-8/
 unzip -o -qq Temp/OpenCore-Series-9.zip -d OpenCore/Series-9/
 unzip -o -qq Temp/OpenCore-Series-100.zip -d OpenCore/Series-100/
 unzip -o -qq Temp/OpenCore-Series-200.zip -d OpenCore/Series-200/
+unzip -o -qq Temp/OpenCore-Series-300.zip -d OpenCore/Series-300/
+unzip -o -qq Temp/OpenCore-Series-400.zip -d OpenCore/Series-400/
+unzip -o -qq Temp/OpenCore-Series-500.zip -d OpenCore/Series-500/
 
 unzip -o -qq Temp/AirportBrcmFixup-2.1.3-RELEASE.zip -d Kexts/
 unzip -o -qq Temp/AppleALC-1.6.3-RELEASE.zip -d Kexts/
@@ -88,9 +128,21 @@ unzip -o -qq Temp/WhateverGreen-1.5.2-RELEASE.zip -d Kexts/
 
 echo "Organising Files..."
 
+rm -rf Clover/Series-5/__MACOSX
+rm -rf Clover/Series-6/__MACOSX
+rm -rf Clover/Series-7/__MACOSX
+rm -rf Clover/Series-8/__MACOSX
+rm -rf Clover/Series-9/__MACOSX
+
+rm -rf OpenCore/Series-6/__MACOSX
+rm -rf OpenCore/Series-7/__MACOSX
+rm -rf OpenCore/Series-8/__MACOSX
 rm -rf OpenCore/Series-9/__MACOSX
 rm -rf OpenCore/Series-100/__MACOSX
 rm -rf OpenCore/Series-200/__MACOSX
+rm -rf OpenCore/Series-300/__MACOSX
+rm -rf OpenCore/Series-400/__MACOSX
+rm -rf OpenCore/Series-500/__MACOSX
 
 cp -rf OpenCore/Update/X64/EFI OpenCore/Update/
 rm -rf OpenCore/Update/Docs
