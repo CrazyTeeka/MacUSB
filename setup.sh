3d2b0b92995fd5e2e91e1528dc05f98f1dad4cea
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2021-08-31 11:57:04 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CloverVersion="5138"
 OpenCoreVersion="0.7.2"
 
 DSDT="https://github.com/CrazyTeeka/MacUSB/raw/master/DSDT"
@@ -34,16 +35,22 @@ mkdir -p Clover/Series-6
 mkdir -p Clover/Series-7
 mkdir -p Clover/Series-8
 mkdir -p Clover/Series-9
+mkdir -p Clover/Series-10
+mkdir -p Clover/Series-20
+mkdir -p Clover/Series-30
+mkdir -p Clover/Series-40
+mkdir -p Clover/Series-50
+mkdir -p Clover/Update
 
 mkdir -p OpenCore/Series-6
 mkdir -p OpenCore/Series-7
 mkdir -p OpenCore/Series-8
 mkdir -p OpenCore/Series-9
-mkdir -p OpenCore/Series-100
-mkdir -p OpenCore/Series-200
-mkdir -p OpenCore/Series-300
-mkdir -p OpenCore/Series-400
-mkdir -p OpenCore/Series-500
+mkdir -p OpenCore/Series-10
+mkdir -p OpenCore/Series-20
+mkdir -p OpenCore/Series-30
+mkdir -p OpenCore/Series-40
+mkdir -p OpenCore/Series-50
 mkdir -p OpenCore/Update
 
 mkdir -p Scripts/Linux
@@ -51,22 +58,28 @@ mkdir -p Scripts/Mac
 
 echo "Downloading Files..."
 
+wget -q -P Temp/ $CLOVER/Clover-$CloverVersion.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-5.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-6.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-7.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-8.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-9.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-10.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-20.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-30.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-40.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-50.zip
 
 wget -q -P Temp/ $OPENCORE/OpenCore-$OpenCoreVersion-RELEASE.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-6.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-7.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-8.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-9.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-100.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-200.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-300.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-400.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-500.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-10.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-20.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-30.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-40.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-50.zip
 
 wget -q -P Tools/ $CLOVER/Clover-Configurator.zip
 wget -q -P Tools/ $OPENCORE/OpenCore-Configurator.zip
@@ -84,8 +97,7 @@ wget -q -P Temp/ $KEXTS/RestrictEvents-1.0.3-RELEASE.zip
 wget -q -P Temp/ $KEXTS/VirtualSMC-1.2.6-RELEASE.zip
 wget -q -P Temp/ $KEXTS/WhateverGreen-1.5.2-RELEASE.zip
 
-wget -q -P Scripts/Linux/ $SCRIPTS/Linux/update
-wget -q -P Scripts/Linux/ $SCRIPTS/Linux/zip
+wget -q -P Scripts/Linux/ $SCRIPTS/Linux/update-opencore.sh
 
 wget -q -P Scripts/Mac/ $SCRIPTS/Mac/backup
 wget -q -P Scripts/Mac/ $SCRIPTS/Mac/backup-efi
@@ -99,22 +111,28 @@ wget -q -P Scripts/Mac/ $SCRIPTS/Mac/trim-enable
 
 echo "Unpacking Files..."
 
+unzip -o -qq Temp/Clover-$CloverVersion.zip -d Clover/Update/
 unzip -o -qq Temp/Clover-Series-5.zip -d Clover/Series-5/
 unzip -o -qq Temp/Clover-Series-6.zip -d Clover/Series-6/
 unzip -o -qq Temp/Clover-Series-7.zip -d Clover/Series-7/
 unzip -o -qq Temp/Clover-Series-8.zip -d Clover/Series-8/
 unzip -o -qq Temp/Clover-Series-9.zip -d Clover/Series-9/
+unzip -o -qq Temp/Clover-Series-10.zip -d Clover/Series-10/
+unzip -o -qq Temp/Clover-Series-20.zip -d Clover/Series-20/
+unzip -o -qq Temp/Clover-Series-30.zip -d Clover/Series-30/
+unzip -o -qq Temp/Clover-Series-40.zip -d Clover/Series-40/
+unzip -o -qq Temp/Clover-Series-50.zip -d Clover/Series-50/
 
 unzip -o -qq Temp/OpenCore-$OpenCoreVersion-RELEASE.zip -d OpenCore/Update/
 unzip -o -qq Temp/OpenCore-Series-6.zip -d OpenCore/Series-6/
 unzip -o -qq Temp/OpenCore-Series-7.zip -d OpenCore/Series-7/
 unzip -o -qq Temp/OpenCore-Series-8.zip -d OpenCore/Series-8/
 unzip -o -qq Temp/OpenCore-Series-9.zip -d OpenCore/Series-9/
-unzip -o -qq Temp/OpenCore-Series-100.zip -d OpenCore/Series-100/
-unzip -o -qq Temp/OpenCore-Series-200.zip -d OpenCore/Series-200/
-unzip -o -qq Temp/OpenCore-Series-300.zip -d OpenCore/Series-300/
-unzip -o -qq Temp/OpenCore-Series-400.zip -d OpenCore/Series-400/
-unzip -o -qq Temp/OpenCore-Series-500.zip -d OpenCore/Series-500/
+unzip -o -qq Temp/OpenCore-Series-10.zip -d OpenCore/Series-10/
+unzip -o -qq Temp/OpenCore-Series-20.zip -d OpenCore/Series-20/
+unzip -o -qq Temp/OpenCore-Series-30.zip -d OpenCore/Series-30/
+unzip -o -qq Temp/OpenCore-Series-40.zip -d OpenCore/Series-40/
+unzip -o -qq Temp/OpenCore-Series-50.zip -d OpenCore/Series-50/
 
 unzip -o -qq Temp/AirportBrcmFixup-2.1.3-RELEASE.zip -d Kexts/
 unzip -o -qq Temp/AppleALC-1.6.3-RELEASE.zip -d Kexts/
@@ -133,16 +151,34 @@ rm -rf Clover/Series-6/__MACOSX
 rm -rf Clover/Series-7/__MACOSX
 rm -rf Clover/Series-8/__MACOSX
 rm -rf Clover/Series-9/__MACOSX
+rm -rf Clover/Series-10/__MACOSX
+rm -rf Clover/Series-20/__MACOSX
+rm -rf Clover/Series-30/__MACOSX
+rm -rf Clover/Series-40/__MACOSX
+rm -rf Clover/Series-50/__MACOSX
+
+cp -rf Clover/Update/CloverV2/EFI Clover/Update/
+rm -rf Clover/Update/CloverV2
+
+rm -rf Clover/Update/EFI/CLOVER/ACPI
+rm -rf Clover/Update/EFI/CLOVER/doc
+rm -rf Clover/Update/EFI/CLOVER/drivers
+rm -rf Clover/Update/EFI/CLOVER/kexts
+rm -rf Clover/Update/EFI/CLOVER/misc
+rm -rf Clover/Update/EFI/CLOVER/OEM
+rm -rf Clover/Update/EFI/CLOVER/ROM
+rm -rf Clover/Update/EFI/CLOVER/themes
+rm -f  Clover/Update/EFI/CLOVER/config-sample.plist
 
 rm -rf OpenCore/Series-6/__MACOSX
 rm -rf OpenCore/Series-7/__MACOSX
 rm -rf OpenCore/Series-8/__MACOSX
 rm -rf OpenCore/Series-9/__MACOSX
-rm -rf OpenCore/Series-100/__MACOSX
-rm -rf OpenCore/Series-200/__MACOSX
-rm -rf OpenCore/Series-300/__MACOSX
-rm -rf OpenCore/Series-400/__MACOSX
-rm -rf OpenCore/Series-500/__MACOSX
+rm -rf OpenCore/Series-10/__MACOSX
+rm -rf OpenCore/Series-20/__MACOSX
+rm -rf OpenCore/Series-30/__MACOSX
+rm -rf OpenCore/Series-40/__MACOSX
+rm -rf OpenCore/Series-50/__MACOSX
 
 cp -rf OpenCore/Update/X64/EFI OpenCore/Update/
 rm -rf OpenCore/Update/Docs
@@ -151,17 +187,9 @@ rm -rf OpenCore/Update/Utilities
 rm -rf OpenCore/Update/X64
 
 rm -rf OpenCore/Update/EFI/OC/ACPI
+rm -rf OpenCore/Update/EFI/OC/Drivers
 rm -rf OpenCore/Update/EFI/OC/Kexts
 rm -rf OpenCore/Update/EFI/OC/Resources
-rm -f  OpenCore/Update/EFI/OC/Drivers/AudioDxe.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/HiiDatabase.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/OpenHfsPlus.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/OpenPartitionDxe.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/OpenUsbKbDxe.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/Ps2KeyboardDxe.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/Ps2MouseDxe.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/UsbMouseDxe.efi
-rm -f  OpenCore/Update/EFI/OC/Drivers/XhciDxe.efi
 
 mkdir -p Kexts/Kexts/VirtualSMC.kext/Contents/Plugins
 
@@ -183,6 +211,6 @@ rm -rf Kexts/WhateverName.app
 rm -f  Kexts/alc-verb
 
 chmod -R +x Scripts/*
-rm -rf Temp
+#rm -rf Temp
 
 echo "Done"
