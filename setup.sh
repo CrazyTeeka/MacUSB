fc92510b80bb5e6fe51d8c408cef8afb6b1346a8
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2022-01-18 15:53:33 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -19,7 +19,6 @@ mkdir -p Kexts
 mkdir -p Scripts
 mkdir -p Temp
 mkdir -p Tools
-mkdir -p Uploads
 
 rm -rf Clover/*
 rm -rf OpenCore/*
@@ -28,29 +27,31 @@ rm -rf Kexts/*
 rm -rf Scripts/*
 rm -rf Temp/*
 rm -rf Tools/*
-rm -rf Uploads/*
 
 mkdir -p Clover/Series-5
 mkdir -p Clover/Series-6
 mkdir -p Clover/Series-7
 mkdir -p Clover/Series-8
 mkdir -p Clover/Series-9
-mkdir -p Clover/Series-10
-mkdir -p Clover/Series-20
-mkdir -p Clover/Series-30
-mkdir -p Clover/Series-40
-mkdir -p Clover/Series-50
+mkdir -p Clover/Series-100
+mkdir -p Clover/Series-200
+mkdir -p Clover/Series-300
+mkdir -p Clover/Series-400
+mkdir -p Clover/Series-500
+mkdir -p Clover/Series-600
+mkdir -p Clover/Themes
 mkdir -p Clover/Update
 
 mkdir -p OpenCore/Series-6
 mkdir -p OpenCore/Series-7
 mkdir -p OpenCore/Series-8
 mkdir -p OpenCore/Series-9
-mkdir -p OpenCore/Series-10
-mkdir -p OpenCore/Series-20
-mkdir -p OpenCore/Series-30
-mkdir -p OpenCore/Series-40
-mkdir -p OpenCore/Series-50
+mkdir -p OpenCore/Series-100
+mkdir -p OpenCore/Series-200
+mkdir -p OpenCore/Series-300
+mkdir -p OpenCore/Series-400
+mkdir -p OpenCore/Series-500
+mkdir -p OpenCore/Series-600
 mkdir -p OpenCore/Update
 
 mkdir -p Scripts/Linux
@@ -64,22 +65,25 @@ wget -q -P Temp/ $CLOVER/Clover-Series-6.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-7.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-8.zip
 wget -q -P Temp/ $CLOVER/Clover-Series-9.zip
-wget -q -P Temp/ $CLOVER/Clover-Series-10.zip
-wget -q -P Temp/ $CLOVER/Clover-Series-20.zip
-wget -q -P Temp/ $CLOVER/Clover-Series-30.zip
-wget -q -P Temp/ $CLOVER/Clover-Series-40.zip
-wget -q -P Temp/ $CLOVER/Clover-Series-50.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-100.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-200.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-300.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-400.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-500.zip
+wget -q -P Temp/ $CLOVER/Clover-Series-600.zip
+wget -q -P Temp/ $CLOVER/Clover-Theme.zip
 
 wget -q -P Temp/ $OPENCORE/OpenCore-$OpenCoreVersion-RELEASE.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-6.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-7.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-8.zip
 wget -q -P Temp/ $OPENCORE/OpenCore-Series-9.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-10.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-20.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-30.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-40.zip
-wget -q -P Temp/ $OPENCORE/OpenCore-Series-50.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-100.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-200.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-300.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-400.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-500.zip
+wget -q -P Temp/ $OPENCORE/OpenCore-Series-600.zip
 
 wget -q -P Tools/ $CLOVER/Clover-Configurator.zip
 wget -q -P Tools/ $OPENCORE/OpenCore-Configurator.zip
@@ -118,22 +122,25 @@ unzip -o -qq Temp/Clover-Series-6.zip -d Clover/Series-6/
 unzip -o -qq Temp/Clover-Series-7.zip -d Clover/Series-7/
 unzip -o -qq Temp/Clover-Series-8.zip -d Clover/Series-8/
 unzip -o -qq Temp/Clover-Series-9.zip -d Clover/Series-9/
-unzip -o -qq Temp/Clover-Series-10.zip -d Clover/Series-10/
-unzip -o -qq Temp/Clover-Series-20.zip -d Clover/Series-20/
-unzip -o -qq Temp/Clover-Series-30.zip -d Clover/Series-30/
-unzip -o -qq Temp/Clover-Series-40.zip -d Clover/Series-40/
-unzip -o -qq Temp/Clover-Series-50.zip -d Clover/Series-50/
+unzip -o -qq Temp/Clover-Series-100.zip -d Clover/Series-100/
+unzip -o -qq Temp/Clover-Series-200.zip -d Clover/Series-200/
+unzip -o -qq Temp/Clover-Series-300.zip -d Clover/Series-300/
+unzip -o -qq Temp/Clover-Series-400.zip -d Clover/Series-400/
+unzip -o -qq Temp/Clover-Series-500.zip -d Clover/Series-500/
+unzip -o -qq Temp/Clover-Series-600.zip -d Clover/Series-600/
+unzip -o -qq Temp/Clover-Theme.zip -d Clover/Themes/
 
 unzip -o -qq Temp/OpenCore-$OpenCoreVersion-RELEASE.zip -d OpenCore/Update/
 unzip -o -qq Temp/OpenCore-Series-6.zip -d OpenCore/Series-6/
 unzip -o -qq Temp/OpenCore-Series-7.zip -d OpenCore/Series-7/
 unzip -o -qq Temp/OpenCore-Series-8.zip -d OpenCore/Series-8/
 unzip -o -qq Temp/OpenCore-Series-9.zip -d OpenCore/Series-9/
-unzip -o -qq Temp/OpenCore-Series-10.zip -d OpenCore/Series-10/
-unzip -o -qq Temp/OpenCore-Series-20.zip -d OpenCore/Series-20/
-unzip -o -qq Temp/OpenCore-Series-30.zip -d OpenCore/Series-30/
-unzip -o -qq Temp/OpenCore-Series-40.zip -d OpenCore/Series-40/
-unzip -o -qq Temp/OpenCore-Series-50.zip -d OpenCore/Series-50/
+unzip -o -qq Temp/OpenCore-Series-100.zip -d OpenCore/Series-100/
+unzip -o -qq Temp/OpenCore-Series-200.zip -d OpenCore/Series-200/
+unzip -o -qq Temp/OpenCore-Series-300.zip -d OpenCore/Series-300/
+unzip -o -qq Temp/OpenCore-Series-400.zip -d OpenCore/Series-400/
+unzip -o -qq Temp/OpenCore-Series-500.zip -d OpenCore/Series-500/
+unzip -o -qq Temp/OpenCore-Series-600.zip -d OpenCore/Series-600/
 
 unzip -o -qq Temp/AirportBrcmFixup-2.1.3-RELEASE.zip -d Kexts/
 unzip -o -qq Temp/AppleALC-1.6.3-RELEASE.zip -d Kexts/
@@ -152,11 +159,12 @@ rm -rf Clover/Series-6/__MACOSX
 rm -rf Clover/Series-7/__MACOSX
 rm -rf Clover/Series-8/__MACOSX
 rm -rf Clover/Series-9/__MACOSX
-rm -rf Clover/Series-10/__MACOSX
-rm -rf Clover/Series-20/__MACOSX
-rm -rf Clover/Series-30/__MACOSX
-rm -rf Clover/Series-40/__MACOSX
-rm -rf Clover/Series-50/__MACOSX
+rm -rf Clover/Series-100/__MACOSX
+rm -rf Clover/Series-200/__MACOSX
+rm -rf Clover/Series-300/__MACOSX
+rm -rf Clover/Series-400/__MACOSX
+rm -rf Clover/Series-500/__MACOSX
+rm -rf Clover/Series-600/__MACOSX
 
 cp -rf Clover/Update/CloverV2/EFI Clover/Update/
 rm -rf Clover/Update/CloverV2
@@ -175,11 +183,12 @@ rm -rf OpenCore/Series-6/__MACOSX
 rm -rf OpenCore/Series-7/__MACOSX
 rm -rf OpenCore/Series-8/__MACOSX
 rm -rf OpenCore/Series-9/__MACOSX
-rm -rf OpenCore/Series-10/__MACOSX
-rm -rf OpenCore/Series-20/__MACOSX
-rm -rf OpenCore/Series-30/__MACOSX
-rm -rf OpenCore/Series-40/__MACOSX
-rm -rf OpenCore/Series-50/__MACOSX
+rm -rf OpenCore/Series-100/__MACOSX
+rm -rf OpenCore/Series-200/__MACOSX
+rm -rf OpenCore/Series-300/__MACOSX
+rm -rf OpenCore/Series-400/__MACOSX
+rm -rf OpenCore/Series-500/__MACOSX
+rm -rf OpenCore/Series-600/__MACOSX
 
 cp -rf OpenCore/Update/X64/EFI OpenCore/Update/
 rm -rf OpenCore/Update/Docs
@@ -210,6 +219,30 @@ rm -rf Kexts/__MACOSX
 rm -rf Kexts/Tools
 rm -rf Kexts/WhateverName.app
 rm -f  Kexts/alc-verb
+
+cp -rf Clover/Themes/Clovy Clover/Series-5/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-6/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-7/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-8/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-9/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-100/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-200/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-300/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-400/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-500/EFI/CLOVER/themes/
+cp -rf Clover/Themes/Clovy Clover/Series-600/EFI/CLOVER/themes/
+
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-5/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-6/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-7/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-8/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-9/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-100/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-200/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-300/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-400/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-500/EFI/CLOVER/config.plist
+sed -i "s/<string>Olarila<\/string>/<string>Clovy<\/string>/g" Clover/Series-600/EFI/CLOVER/config.plist
 
 chmod -R +x Scripts/*
 #rm -rf Temp
